4c1189490e416f19bdeef3b9b044fa2bd12844e1
raquelmgalbuquerque <81765141+raquelmgalbuquerque@users.noreply.github.com>
2023-01-29 22:52:01 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,471 @@
+var students = [
+    {
+        id: 4823,
+        uid: "c336aacc-7d4c-4cbe-b341-e6f96b298b2f",
+        password: "lhTwqmsMEF",
+        first_name: "Erlene",
+        last_name: "Zieme",
+        username: "erlene.zieme",
+        email: "erlene.zieme@email.com",
+        avatar: "https://robohash.org/utdoloribusquibusdam.png?size=300x300&set=set1",
+        gender: "Genderfluid",
+        phone_number: "+972 672-700-5268",
+        social_insurance_number: "439215005",
+        date_of_birth: "1974-06-20",
+        employment: {
+            title: "Banking Developer",
+            key_skill: "Work under pressure"
+        },
+        address: {
+            city: "O'Reillymouth",
+            street_name: "Ericka Estates",
+            street_address: "79231 Elouise Brooks",
+            zip_code: "46313-2642",
+            state: "Porto",
+            country: "Portugal",
+            coordinates: {
+                lat: 83.78180013934258,
+                lng: 72.09076464229105
+            }
+        },
+        credit_card: {
+            cc_number: "6771-8936-2356-1810"
+        },
+        subscription: {
+            plan: "Silver",
+            status: "Active",
+            payment_method: "Bitcoins",
+            term: "Payment in advance"
+        }
+    },
+    {
+        id: 8243,
+        uid: "b6b8a35e-0a24-43cd-83e6-29b5947f444b",
+        password: "qn26ZxVi4t",
+        first_name: "Linwood",
+        last_name: "VonRueden",
+        username: "linwood.vonrueden",
+        email: "linwood.vonrueden@email.com",
+        avatar: "https://robohash.org/suntnamat.png?size=300x300&set=set1",
+        gender: "Male",
+        phone_number: "+1-758 (860) 185-1077",
+        social_insurance_number: "672424421",
+        date_of_birth: "1997-12-25",
+        employment: {
+            title: "Corporate Advertising Consultant",
+            key_skill: "Teamwork"
+        },
+        address: {
+            city: "Port Sirena",
+            street_name: "Hartmann Road",
+            street_address: "3185 Golden Plain",
+            zip_code: "41302",
+            state: "Madrid",
+            country: "Spain",
+            coordinates: {
+                lat: -53.071088716227884,
+                lng: 29.41031842893699
+            }
+        },
+        credit_card: {
+            cc_number: "4651961422033"
+        },
+        subscription: {
+            plan: "Professional",
+            status: "Pending",
+            payment_method: "Money transfer",
+            term: "Monthly"
+        }
+    },
+    {
+        id: 6932,
+        uid: "4ba1c7dc-3563-46b9-baa5-02009e0a3896",
+        password: "IaVKtq3UsP",
+        first_name: "Fay",
+        last_name: "Simonis",
+        username: "fay.simonis",
+        email: "fay.simonis@email.com",
+        avatar: "https://robohash.org/consequaturadnon.png?size=300x300&set=set1",
+        gender: "Genderfluid",
+        phone_number: "+374 (782) 622-8864 x2068",
+        social_insurance_number: "292013109",
+        date_of_birth: "1988-05-05",
+        employment: {
+            title: "Marketing Facilitator",
+            key_skill: "Networking skills"
+        },
+        address: {
+            city: "West Sammieton",
+            street_name: "Jakubowski Mount",
+            street_address: "186 Hackett Estate",
+            zip_code: "06579",
+            state: "Wisconsin",
+            country: "United States",
+            coordinates: {
+                lat: -70.30868502117225,
+                lng: -147.73260482758332
+            }
+        },
+        credit_card: {
+            cc_number: "4701-9237-3149-2762"
+        },
+        subscription: {
+            plan: "Basic",
+            status: "Idle",
+            payment_method: "Visa checkout",
+            term: "Monthly"
+        }
+    },
+    {
+        id: 7309,
+        uid: "48c6604d-0595-4ef7-b4a2-3b87f2cf200e",
+        password: "fv2LKnVSt7",
+        first_name: "Libby",
+        last_name: "Block",
+        username: "libby.block",
+        email: "libby.block@email.com",
+        avatar: "https://robohash.org/voluptatemquasiconsectetur.png?size=300x300&set=set1",
+        gender: "Genderfluid",
+        phone_number: "+378 973.738.4330 x280",
+        social_insurance_number: "990388167",
+        date_of_birth: "1991-10-31",
+        employment: {
+            title: "Human Farming Liaison",
+            key_skill: "Problem solving"
+        },
+        address: {
+            city: "McCulloughville",
+            street_name: "Kirby Locks",
+            street_address: "8131 Boyle Hill",
+            zip_code: "09282-0927",
+            state: "New Jersey",
+            country: "United States",
+            coordinates: {
+                lat: 29.76340537719348,
+                lng: -133.4789541869037
+            }
+        },
+        credit_card: {
+            cc_number: "4464283569501"
+        },
+        subscription: {
+            plan: "Gold",
+            status: "Active",
+            payment_method: "Apple Pay",
+            term: "Monthly"
+        }
+    },
+    {
+        id: 9291,
+        uid: "13c6335a-aea3-4d16-9699-ab2bb42d7cf2",
+        password: "S3JLDx2EFG",
+        first_name: "Jarod",
+        last_name: "Russel",
+        username: "jarod.russel",
+        email: "jarod.russel@email.com",
+        avatar: "https://robohash.org/quinecessitatibusdoloremque.png?size=300x300&set=set1",
+        gender: "Genderqueer",
+        phone_number: "+33 432.335.9849",
+        social_insurance_number: "990304867",
+        date_of_birth: "1963-10-10",
+        employment: {
+            title: "National Community-Services Producer",
+            key_skill: "Networking skills"
+        },
+        address: {
+            city: "West Kalynmouth",
+            street_name: "Lory Cove",
+            street_address: "8402 Sean Springs",
+            zip_code: "58850",
+            state: "Indiana",
+            country: "United States",
+            coordinates: {
+                lat: 70.75992655079929,
+                lng: 29.987855061899012
+            }
+        },
+        credit_card: {
+            cc_number: "4717353954964"
+        },
+        subscription: {
+            plan: "Free Trial",
+            status: "Idle",
+            payment_method: "Debit card",
+            term: "Monthly"
+        }
+    },
+    {
+        id: 9613,
+        uid: "c747ce65-87af-4417-b279-19ea142fa310",
+        password: "0J1GCak98M",
+        first_name: "Lonnie",
+        last_name: "Bailey",
+        username: "lonnie.bailey",
+        email: "lonnie.bailey@email.com",
+        avatar: "https://robohash.org/utdelenitimaxime.png?size=300x300&set=set1",
+        gender: "Genderqueer",
+        phone_number: "+253 166.217.4324",
+        social_insurance_number: "350425203",
+        date_of_birth: "1982-11-09",
+        employment: {
+            title: "Mining Analyst",
+            key_skill: "Self-motivated"
+        },
+        address: {
+            city: "North Obduliaton",
+            street_name: "Margy Centers",
+            street_address: "24219 Weimann Mountain",
+            zip_code: "94906-7558",
+            state: "West Virginia",
+            country: "United States",
+            coordinates: {
+                lat: 67.82083695670224,
+                lng: -23.792752393881216
+            }
+        },
+        credit_card: {
+            cc_number: "6771-8912-0978-7684"
+        },
+        subscription: {
+            plan: "Diamond",
+            status: "Blocked",
+            payment_method: "Google Pay",
+            term: "Monthly"
+        }
+    },
+    {
+        id: 7569,
+        uid: "a927f1ab-d528-48aa-98c6-7ff6c9004260",
+        password: "sKdruYxGPA",
+        first_name: "Cyndy",
+        last_name: "Maggio",
+        username: "cyndy.maggio",
+        email: "cyndy.maggio@email.com",
+        avatar: "https://robohash.org/quiatotamrem.png?size=300x300&set=set1",
+        gender: "Female",
+        phone_number: "+967 1-468-066-5612 x40195",
+        social_insurance_number: "903947117",
+        date_of_birth: "1990-09-17",
+        employment: {
+            title: "Manufacturing Strategist",
+            key_skill: "Problem solving"
+        },
+        address: {
+            city: "New Britteny",
+            street_name: "Terence Plains",
+            street_address: "6008 Shane Junction",
+            zip_code: "76301",
+            state: "Rio de Janeiro",
+            country: "Brazil",
+            coordinates: {
+                lat: 14.96125625710387,
+                lng: 10.71797505318085
+            }
+        },
+        credit_card: {
+            cc_number: "5570-4713-0966-6883"
+        },
+        subscription: {
+            plan: "Silver",
+            status: "Idle",
+            payment_method: "Visa checkout",
+            term: "Full subscription"
+        }
+    },
+    {
+        id: 4939,
+        uid: "d6f00da9-f389-4458-9918-2ca6077f7b23",
+        password: "CxtmJRduOT",
+        first_name: "Stefan",
+        last_name: "Hayes",
+        username: "stefan.hayes",
+        email: "stefan.hayes@email.com",
+        avatar: "https://robohash.org/dolorettempore.png?size=300x300&set=set1",
+        gender: "Genderfluid",
+        phone_number: "+353 (254) 478-6538 x599",
+        social_insurance_number: "590494696",
+        date_of_birth: "1958-05-15",
+        employment: {
+            title: "Farming Director",
+            key_skill: "Networking skills"
+        },
+        address: {
+            city: "Lake Dexterport",
+            street_name: "Deloris Courts",
+            street_address: "410 Veum Spring",
+            zip_code: "51767",
+            state: "Paris",
+            country: "France",
+            coordinates: {
+                lat: 38.01403084978256,
+                lng: -104.69649857256239
+            }
+        },
+        credit_card: {
+            cc_number: "4408-3662-1153-5294"
+        },
+        subscription: {
+            plan: "Bronze",
+            status: "Pending",
+            payment_method: "Google Pay",
+            term: "Payment in advance"
+        }
+    },
+    {
+        id: 7114,
+        uid: "c5cb317b-88f8-439b-9f2d-737cc1f73bbe",
+        password: "z3F7gBJHsK",
+        first_name: "Emeline",
+        last_name: "D'Amore",
+        username: "emeline.d'amore",
+        email: "emeline.d'amore@email.com",
+        avatar: "https://robohash.org/asperioresutrepellat.png?size=300x300&set=set1",
+        gender: "Bigender",
+        phone_number: "+266 1-666-071-0564 x510",
+        social_insurance_number: "424116572",
+        date_of_birth: "1981-06-21",
+        employment: {
+            title: "Human Manager",
+            key_skill: "Networking skills"
+        },
+        address: {
+            city: "Stephanville",
+            street_name: "Schneider Crescent",
+            street_address: "231 Clair Junctions",
+            zip_code: "03128-0654",
+            state: "Missouri",
+            country: "United States",
+            coordinates: {
+                lat: -2.567585409703483,
+                lng: -104.38847459730279
+            }
+        },
+        credit_card: {
+            cc_number: "6771-8960-6247-0856"
+        },
+        subscription: {
+            plan: "Free Trial",
+            status: "Blocked",
+            payment_method: "Money transfer",
+            term: "Annual"
+        }
+    },
+    {
+        id: 6080,
+        uid: "91de7f52-32d1-48c2-a403-12250248961e",
+        password: "40kL8xeUrg",
+        first_name: "Wilton",
+        last_name: "Erdman",
+        username: "wilton.erdman",
+        email: "wilton.erdman@email.com",
+        avatar: "https://robohash.org/nemomaioresin.png?size=300x300&set=set1",
+        gender: "Non-binary",
+        phone_number: "+1-284 289.649.8979 x7433",
+        social_insurance_number: "925196248",
+        date_of_birth: "1995-05-11",
+        employment: {
+            title: "Future Community-Services Engineer",
+            key_skill: "Teamwork"
+        },
+        address: {
+            city: "West Barriehaven",
+            street_name: "Rob Knolls",
+            street_address: "442 Reiko Trafficway",
+            zip_code: "78665",
+            state: "Lisboa",
+            country: "Portugal",
+            coordinates: {
+                lat: -16.358328925759878,
+                lng: -18.429763048778796
+            }
+        },
+        credit_card: {
+            cc_number: "6771-8926-9414-6311"
+        },
+        subscription: {
+            plan: "Essential",
+            status: "Blocked",
+            payment_method: "Cheque",
+            term: "Full subscription"
+        }
+    },
+];
+var flags = [
+    {
+        country: "Portugal",
+        flag: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Flag_of_Portugal.svg"
+    },
+    {
+        country: "France",
+        flag: "https://upload.wikimedia.org/wikipedia/commons/1/18/Flag_of_France_%28lighter_variant%29.svg"
+    },
+    {
+        country: "United States",
+        flag: "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
+    },
+    {
+        country: "Spain",
+        flag: "https://upload.wikimedia.org/wikipedia/commons/8/89/Bandera_de_Espa%C3%B1a.svg"
+    },
+    {
+        country: "Brazil",
+        flag: "https://upload.wikimedia.org/wikipedia/en/0/05/Flag_of_Brazil.svg"
+    },
+];
+// ALINEA 2
+function getFlag(studentObject, flagsArray) {
+    var studentFlag = flagsArray.find(function (flag) { return flag.country === studentObject.address.country; });
+    return studentFlag === null || studentFlag === void 0 ? void 0 : studentFlag.flag;
+}
+// ALINEA 3
+function getAge(studentObject) {
+    var currentYear = new Date().getFullYear();
+    var birthday = new Date(studentObject.date_of_birth).getFullYear();
+    return currentYear - birthday;
+}
+var newStudents = students.map(function (student) {
+    return {
+        fullName: "".concat(student.first_name, " ").concat(student.last_name),
+        gender: student.gender,
+        country: student.address.country,
+        city: student.address.state,
+        flag: getFlag(student, flags),
+        age: getAge(student)
+    };
+});
+// ALINEA 4
+var studentsContainer = document.querySelector(".students-container");
+newStudents.forEach(function (student) {
+    var studentElement = document.createElement("div");
+    studentElement.setAttribute("class", "student-element");
+    studentElement.innerHTML = "\n  <img src=\"".concat(student.flag, "\" alt=\"").concat(student.country, " flag\" class=\"student-flag\">\n  <div>\n  <div><b>Name:</b> ").concat(student.fullName, "</div>\n  <div><b>Gender:</b> ").concat(student.gender, "</div>\n  <div><b>Country:</b> ").concat(student.country, "</div>\n  <div><b>City:</b> ").concat(student.city, "</div>\n  <div><b>Age:</b> ").concat(student.age, "</div>\n  </div>\n  ");
+    studentsContainer === null || studentsContainer === void 0 ? void 0 : studentsContainer.appendChild(studentElement);
+});
+// Just checking...
+// console.log(newStudents);
+// ALINEA 5
+function getAverageAge(array) {
+    var totalAge = array.reduce(function (acc, curr) { return acc + curr.age; }, 0);
+    var totalStudents = array.length;
+    var averageAge = totalAge / totalStudents;
+    return averageAge;
+}
+function studentsPerCountry(array) {
+    var countryCounter = {};
+    var studentsPerCountryString = "";
+    array.forEach(function (object) {
+        if (object.country in countryCounter) {
+            countryCounter[object.country]++;
+        }
+        else {
+            countryCounter[object.country] = 1;
+        }
+    });
+    for (var key in countryCounter) {
+        studentsPerCountryString =
+            studentsPerCountryString + "".concat(key, ": ").concat(countryCounter[key], " | ");
+    }
+    return studentsPerCountryString.slice(0, -2);
+}
+var studentsDataContainer = document.querySelector(".students-data-container");
+studentsDataContainer.innerHTML = "<div><b>Average age:</b> ".concat(getAverageAge(newStudents), "</div>\n<div><b>Students per country:</b> ").concat(studentsPerCountry(newStudents), "</div>");
